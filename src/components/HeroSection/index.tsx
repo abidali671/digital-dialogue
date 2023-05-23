029da5cb92491daf32d610d595aff6a7bc53b7a5
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-05-23 14:44:08 -0700

    fix: absolute

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -5,31 +5,31 @@ import BlogImage from "../../assest/BlogImage.png";
 const Hero = () => {
   return (
     <div className="flex justify-center h-[calc(100vh_-_64px)] overflow-hidden">
-      <div className="w-[40%] relative"></div>
+      <div className="w-[40%] relative bg-gray-50 md:block  hidden"></div>
       <div className="w-[60%]">
-        <Image src={BlogImage} alt="Blogy logo" className="w-[100%] h-[100%]" />
+        <Image src={BlogImage} alt="Blogy logo" className="w-[100%] h-[100%] md:block  hidden " />
       </div>
       <div>
-        <div className="bg-gray-200 absolute w-[45%] left-24 top-48  p-10 flex flex-col gap-4">
-          <span className="flex items-center gap-1">
-            <hr className="w-10 h-[2px]  bg-gray-100 border-0 rounded bg-orange-700" />
+        <div className="bg-white absolute w-[100%] md:w-[50%] sm:text-left text-center sm:items-start items-center   md:left-20 left-0 top-24  p-6 flex flex-col gap-2">
+          <span className="flex items-center gap-1 ">
+            <hr className="w-10 h-[2px]   border-0 rounded bg-orange-700" />
             <p>Interior</p>
           </span>
-          <h2 className="text-5xl font-bold font-serif leading-[57px]">
+          <h2 className="text-4xl lg:text-5xl font-bold font-PT ">
             How to Get Started With Interior Design
           </h2>
-          <p className="text-gray-500 flex items-center gap-2">
-            March 25, 2021{" "}
+          <p className="text-gray-500 flex items-center gap-2 text-[12px] lg:text-[16px]">
+            March 25, 2021
             <span className="h-[5px] w-[5px] bg-gray-500 rounded-lg"></span> 4
             min read
           </p>
-          <p className="text-gray-500 ">
+          <p className="text-gray-500 text-[12px] lg:text-[16px]">
             Nulla et commodo turpis. Etiam hendrerit ornare pharetra. Cras
             eleifend purus vitae lorem venenatis bibendum. Sed commodo mi quis
             augue finibus, ut feugiat erat aliquam.
           </p>
 
-          <button className="p-3 bg-orange-500 rounded-sm w-[140px] text-white">
+          <button className="p-2 bg-orange-500 rounded-sm w-[130px] text-white">
             Read More
           </button>
         </div>
