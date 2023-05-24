0f0a0e41a5452891dd08dcc11bd7d4a8c54e3dd6
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-05-24 15:27:11 -0700

    add card component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./HeroSection";
+export { default as Card } from "./Card";
+export { default as LatestPost } from "./LatestPost";
