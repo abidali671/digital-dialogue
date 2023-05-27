f200ee7db4a46c07584f02b23462a8e4c119b415
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-05-27 15:06:09 -0700

    add logo svg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['media.cnn.com'],
+        domains: ['media.cnn.com', 'freepngimg.com'],
     },
 }
 
