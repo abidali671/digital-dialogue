373a6ede69d46054bd6177716c105c43162c58ac
abidali671 <abid.saeed.ali92@gmail.com>
2023-05-30 15:40:47 +0500

    update home page hero section

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,102 +1,125 @@
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import BlogImage from "../../assest/BlogImage.png";
-import { slideContent, slideContentPropsT } from "./heroContent";
+import ContentContainer from "../ContentContainer";
+import { slideContent } from "./heroContent";
+
 const Hero = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [content, setContent] = useState<slideContentPropsT>()
+  const content = useMemo(() => slideContent[currentSlide], [currentSlide]);
 
-  const handlePrevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? slideContent.length - 1 : currentSlide - 1);
-  };
+  const handlePrevSlide = useCallback(() => {
+    setCurrentSlide(
+      currentSlide === 0 ? slideContent.length - 1 : currentSlide - 1
+    );
+  }, []);
 
-  useEffect(() => {
-    setContent(slideContent[currentSlide])
-  }, [currentSlide])
+  const handleNextSlide = useCallback(() => {
+    setCurrentSlide(
+      currentSlide === slideContent.length - 1 ? 0 : currentSlide + 1
+    );
+  }, [currentSlide]);
 
-
-  const handleNextSlide = () => {
-    setCurrentSlide(currentSlide === slideContent.length - 1 ? 0 : currentSlide + 1);
-  };
   return (
     <div className="flex relative justify-center sm:h-[calc(100vh_-_64px)] h-[70vh] overflow-hidden">
-      <div className="w-[40%] relative md: bg-neutral-50 bg-none ">
-
-      </div>
+      <div className="w-[40%] relative md: bg-neutral-50 bg-none "></div>
       <div className="w-[60%]">
         <Image
           src={BlogImage}
           alt="Blogy logo"
-          className="w-[100%] h-[100%] md:block  hidden "
+          className="w-[100%] h-[100%] md:block hidden "
         />
       </div>
-      <div className="flex h-full items-center">
-        <div key={content?.id} className="bg-white z-0 absolute  md:w-[46%]  w-[100%]  sm:text-left text-center sm:items-start  items-center left-0 md:left-24  p-6 flex flex-col gap-2">
-          <span className="flex items-center gap-1 ">
-            <hr className="w-10 h-[2px]   border-0 rounded bg-orange-700" />
-            <p>{content?.tag}</p>
-          </span>
-          <h2 className="text-4xl lg:text-5xl  font-bold  ">
-            {content?.title}
-          </h2>
-          <p className="text-gray-500 flex items-center gap-2 text-[12px] lg:text-[16px]">
-            {content?.date}
-            <span className="h-[5px] w-[5px] bg-gray-500 rounded-lg"></span> 4  min read
-          </p>
-          <p className="text-gray-500 text-[12px] lg:text-[16px]">
-            {content?.description}
-          </p>
-          <button onClick={() => console.log(content?.id)} className="p-2 bg-orange-500 rounded-sm w-[100px]  lg:w-[130px] text-white">
-            Read More
-          </button>
-        </div>
-      </div>
-
-      <div className="flex  absolute lg:bottom-6 bottom-2 sm:left-24 items-center gap-3">
-        {/* content slider */}
-        <div onClick={handlePrevSlide} className="p-2 rounded-full text-center cursor-pointer bg-white shadow-md text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
+      <ContentContainer className="!absolute top-0 bottom-0 left-[50%] translate-x-[-50%]">
+        <div className="absolute left-0 top-[50%] translate-y-[-50%] flex flex-col gap-10 max-w-[650px] width-[90%]">
+          <div
+            key={content?.id}
+            className="bg-white z-0 shadow-lg w-[100%] sm:text-left text-center sm:items-start items-center p-6 flex flex-col gap-2"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
+            <span className="flex items-center gap-1 ">
+              <hr className="w-10 h-[2px]   border-0 rounded bg-orange-700" />
+              <p>{content?.tag}</p>
+            </span>
+            <h2 className="text-4xl lg:text-5xl  font-bold  ">
+              {content?.title}
+            </h2>
+            <p className="text-gray-500 flex items-center gap-2 text-[12px] lg:text-[16px]">
+              {content?.date}
+              <span className="h-[5px] w-[5px] bg-gray-500 rounded-lg"></span> 4
+              min read
+            </p>
+            <p className="text-gray-500 text-[12px] lg:text-[16px]">
+              {content?.description}
+            </p>
+            <button
+              onClick={() => console.log(content?.id)}
+              className="p-2 bg-orange-500 rounded-sm w-[100px]  lg:w-[130px] text-white"
+            >
+              Read More
+            </button>
+          </div>
+          <SliderIndicator
+            onPrevSlide={handlePrevSlide}
+            onNextSlide={handleNextSlide}
+            content={content}
+          />
         </div>
-        <div className="flex gap-1">
-          {slideContent.map(((item) => (
-            <span key={item.id} className={`h-2 w-2 rounded-full ${item.id == content?.id ? 'bg-orange-200' : ' bg-orange-400'}`}></span>
-          )))}
-
-        </div>
-        <div onClick={handleNextSlide} className="p-2 rounded-full text-center cursor-pointer bg-white shadow-md text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </div>
-        {/* content slider end*/}
-      </div>
-
+      </ContentContainer>
     </div>
   );
 };
+
+const SliderIndicator = ({ onPrevSlide, onNextSlide, content }) => (
+  <div className="flex items-center gap-3">
+    <div
+      onClick={onPrevSlide}
+      className="p-2 rounded-full text-center cursor-pointer bg-white shadow-md text-black"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 19.5L8.25 12l7.5-7.5"
+        />
+      </svg>
+    </div>
+    <div className="flex gap-1">
+      {slideContent.map((item) => (
+        <span
+          key={item.id}
+          className={`h-2 w-2 rounded-full ${
+            item.id == content?.id ? "bg-orange-200" : " bg-orange-400"
+          }`}
+        ></span>
+      ))}
+    </div>
+    <div
+      onClick={onNextSlide}
+      className="p-2 rounded-full text-center cursor-pointer bg-white shadow-md text-black"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M8.25 4.5l7.5 7.5-7.5 7.5"
+        />
+      </svg>
+    </div>
+  </div>
+);
 
 export default Hero;
