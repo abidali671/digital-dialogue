17a2b90f25a10d96d369875d4a4fd712b84b2b20
abidali671 <abid.saeed.ali92@gmail.com>
2023-05-31 17:16:29 +0500

    featured section f-width on mobile

--- src/components/featuredSection/index.tsx
+++ src/components/featuredSection/index.tsx
@@ -10,7 +10,7 @@ const FeaturedSection = () => {
           <h2 className="font-bold text-4xl ">Featured Post</h2>
           <hr className="w-24 h-[4px]   border-1 rounded bg-orange-700" />
         </span>
-        <div className="py-10 grid place-items-center gap-4 w-[70%] mx-auto grid-cols-[repeat(auto-fill,minmax(300px,1fr))] ">
+        <div className="py-10 grid place-items-center gap-4  w-full sm:w-[70%] mx-auto grid-cols-[repeat(auto-fill,minmax(300px,1fr))] ">
           {Array(2).fill(
             <Card
               imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/allbirds-sneakers-review-wool-runnerjpg.jpg?q=h_1090,w_1938,x_0,y_0"
