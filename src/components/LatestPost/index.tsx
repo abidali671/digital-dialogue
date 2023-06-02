e65ad8fe84474ae2eaec9647bc827dcffb381012
abidali671 <abid.saeed.ali92@gmail.com>
2023-06-02 15:57:54 +0500

    uodate slide content

--- src/components/LatestPost/index.tsx
+++ src/components/LatestPost/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../Card";
 import ContentContainer from "../ContentContainer";
+import { slideContent } from "../HeroSection/heroContent";
 
 const LatestPost = () => {
   return (
@@ -12,15 +13,17 @@ const LatestPost = () => {
             <hr className="w-24 h-[4px]   border-1 rounded bg-orange-700" />
           </span>
           <div className="py-10  grid place-items-center gap-6 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
-            {Array(6).fill(
+            {slideContent.map((item) => (
               <Card
+                key={item.id}
                 imageUrl="https://freepngimg.com/thumb/shoes/27428-5-nike-shoes-transparent-background-thumb.png"
                 altText="shoes"
-                title="Top 10 beautiful Place in Bangladesh"
-                description="Nulla et commodo turpis. Etiam hendrerit ornare pharetra."
-                date="March 25, 2021"
+                title={item.title}
+                description={item.description.substring(0, 40)}
+                date={item.date}
+                id={item.id}
               />
-            )}
+            ))}
           </div>
         </ContentContainer>
       </div>
