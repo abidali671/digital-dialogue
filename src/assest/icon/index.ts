184cab4031490f72ece9c464f8d0d6d044b5eb3d
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-06-05 13:34:03 -0700

    svg icons add

---
+++ src/assest/icon/index.ts
@@ -0,0 +1,4 @@
+export { default as Facebook } from '@/assest/icon/facebook'
+export { default as Twitter } from '@/assest/icon/twitter'
+export { default as Pinterest } from '@/assest/icon/pinterest'
+export { default as LinkIcon } from '@/assest/icon/link'
