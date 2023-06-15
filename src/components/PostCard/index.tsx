d1604d83b9c974cbef67b4f5edd4f18bce4d44aa
abidali671 <abid.saeed.ali92@gmail.com>
2023-06-15 12:45:24 +0500

    ui updates

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -31,17 +31,19 @@ const PostCard = ({ data }: CardPropsT) => {
           />
         )}
       </div>
-      <div className="p-4 gap-1 flex flex-col  text-left items-start border border-gray-200 shadow-gray-200 shadow-md border-t-0 bg-white sm:relative sm:w-[95%] sm:top-[-20px]">
+      <div className="p-4 gap-2 flex flex-col  text-left items-start border border-gray-200 shadow-gray-200 shadow-md border-t-0 bg-white sm:relative sm:w-[95%] sm:top-[-20px]">
         <span className="flex items-center gap-1">
           <hr className="w-10 h-[2px] border-0 rounded bg-orange-700" />
           <p>{category.fields.label}</p>
         </span>
         <Link href="#">
-          <h5 className="text-[28px] font-bold tracking-tight text-gray-900 leading-[32px] font-PT">
+          <h5 className="text-[28px] font-bold tracking-tight text-gray-900 leading-[32px] line-clamp-2 h-[64px] font-PT">
             {title}
           </h5>
         </Link>
-        <p className="text-gray-500 text-[12px] lg:text-[16px]">{exerpt}</p>
+        <p className="text-gray-500 text-[12px] lg:text-[16px] line-clamp-5 h-[92px]">
+          {exerpt}
+        </p>
         <p className="text-gray-500 flex items-center gap-2 text-[12px] lg:text-[16px]">
           {moment(createdAt).format("MMMM DD, YYYY")}
           <span className="h-[5px] w-[5px] bg-gray-500 rounded-lg"></span>{" "}
@@ -53,10 +55,10 @@ const PostCard = ({ data }: CardPropsT) => {
             pathname: "/blogs/[category]/[blog_detail]",
             query: { category: category.fields.slug, blog_detail: slug },
           }}
-          className="pt-2 inline-flex items-center text-md font-medium text-center text-black"
+          className="pt-2 gap-2 inline-flex items-center text-md font-medium text-center text-black whitespace-nowrap"
         >
           Read Article
-          <Arrow />
+          <Arrow height={24} width={24} />
         </Link>
       </div>
     </div>
