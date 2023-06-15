91f38be2bb6c1045ab97a1a6d1fc2933e16d2cf6
abidali671 <abid.saeed.ali92@gmail.com>
2023-06-15 13:41:53 +0500

    update lock file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 export { default as Navbar } from "./Container/Navbar";
 export { default as Hero } from "./HeroSection";
 export { default as LatestPost } from "./LatestPost";
-export { default as AllPosts } from "./AllPosts/index";
+export { default as AllPosts } from "./AllPosts";
 export { default as ContentContainer } from "./ContentContainer";
 export { default as PostCard } from "./PostCard";
 export { default as Category } from "./Category";
