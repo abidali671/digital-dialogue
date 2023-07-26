25055ba9e6e6a88d9c0beb8d9453b443b2ceacf9
abidali671 <abid.saeed.ali92@gmail.com>
2023-07-26 16:06:56 +0500

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from "next/script";
 import Layout from "@/components/layout";
 import { AppInitialProps, AppProps } from "next/app";
 import "../styles/global.css";
@@ -12,6 +13,16 @@ export default function MyApp({ Component, pageProps }: AppPropsT) {
   return (
     <React.Fragment>
       <Head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-LQYZDNMNFL" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+        </Script>
         <title>Digital Dialogue</title>
         <link rel="icon" href="/favicon.svg" sizes="any" />
       </Head>
