80c6af72caad5ce3b5f718c91a896a420b1caa4a
abidali671 <abid.saeed.ali92@gmail.com>
2023-08-01 00:29:26 +0500

    fetch blog api

--- src/pages/api/blogs.ts
+++ src/pages/api/blogs.ts
@@ -5,16 +5,18 @@ import config from "@/lib/config";
 const FetchBlogs = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const page_no = Number(req.query.page) || 1;
+    const links_to_entry = req.query.links_to_entry;
 
     const response = await contentful_client.getEntries({
       content_type: "post",
       limit: config.BLOGS_PER_PAGE,
       skip: (page_no - 1) * config.BLOGS_PER_PAGE,
+      links_to_entry,
     });
 
-    res.status(200).json({ data: response.items });
-  } catch (err) {
-    res.status(500).json({ data: err });
+    res.status(200).json({ items: response.items, total: response.total });
+  } catch (error) {
+    res.status(500).json({ error });
   }
 };
 
