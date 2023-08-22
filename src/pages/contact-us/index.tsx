c3ab211755cc0f11ff08488f0ba383988d987261
abidali671 <abid.saeed.ali92@gmail.com>
2023-08-22 17:53:13 +0500

    remove extra code

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,30 +1,8 @@
 import constants from "@/constants";
 import config from "@/lib/config";
 import contentful_client from "@/lib/contentful/client";
-import { ChangeEvent, useState } from "react";
 
 const ContactUs = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Form data submitted:", formData);
-    // You can perform further actions like sending the data to a server her
-  };
   return (
     <div className="grid grid-cols-10 max-w-7xl mx-auto mt-20 min-h-[calc(100vh_-_80px)]">
       <div className="col-span-10 md:col-span-7 flex flex-col gap-10 p-4">
@@ -36,18 +14,14 @@ const ContactUs = () => {
         >
           <h5>Send us a message</h5>
           <input
-            onChange={handleChange}
             type="text"
             placeholder="Name"
             name="name"
-            value={formData.name}
             className="form-input"
             required
           />
           <input
             type="email"
-            onChange={handleChange}
-            value={formData.email}
             placeholder="Email"
             name="email"
             className="form-input"
@@ -55,21 +29,17 @@ const ContactUs = () => {
           />
           <input
             type="subject"
-            onChange={handleChange}
             placeholder="Subject"
             name="subject"
             className="form-input"
-            value={formData.subject}
             required
           />
           <textarea
-            onChange={handleChange}
             placeholder="Enter Your Message"
             name="message"
             className="form-input"
             rows={4}
             required
-            value={formData.message}
           />
           <button className="btn-primary w-fit" type="submit">
             Send
