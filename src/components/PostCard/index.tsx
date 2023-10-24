1db3c45cbf0fa1ccc563a2f05e0f153941bf17f1
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-10-24 15:42:56 -0700

    update post card ui

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import moment from "moment";
-import { Arrow } from "@/assets/icon";
 import { IPostData } from "@/types";
 
 interface CardPropsT {
@@ -10,67 +9,56 @@ interface CardPropsT {
 }
 
 const PostCard = ({ data }: CardPropsT) => {
-  const { category, coverImage, title, excerpt, slug } = data.fields;
+  const { category, coverImage, title, excerpt, slug, author } = data.fields;
   const { createdAt } = data.sys;
+  const { url } = author.fields.picture.fields.file;
 
   return (
-    <div className="post-card-root">
-      <div className="post-card-cover-wrapper">
-        <Image
-          src={"https:" + coverImage.fields.file.url}
-          alt={coverImage.fields.description}
-          width={396}
-          height={240}
-        />
-      </div>
-      <div className="post-card-content-wrapper">
-        <Link
-          href={{
-            pathname: "/blogs/[category]",
-            query: { category: category.fields.slug },
-          }}
-          className="post-card-label-wrapper"
-        >
-          <hr className="post-card-label-line" />
-          <p>{category.fields.label}</p>
-        </Link>
-        <Link
-          href={{
-            pathname: "/blogs/[category]/[blog_detail]",
-            query: { category: category.fields.slug, blog_detail: slug },
-          }}
-          className="post-card-title"
-        >
-          {title}
-        </Link>
-        <Link
-          href={{
-            pathname: "/blogs/[category]/[blog_detail]",
-            query: { category: category.fields.slug, blog_detail: slug },
-          }}
-          className="post-card-excerpt"
-        >
-          {excerpt}
-        </Link>
-        <Link
-          href={{
-            pathname: "/blogs/[category]/[blog_detail]",
-            query: { category: category.fields.slug, blog_detail: slug },
-          }}
-          className="post-card-created-date"
-        >
-          {moment(createdAt).format("MMMM DD, YYYY")}
-        </Link>
+    <div className="max-w-sm flex flex-col flex-wrap overflow-hidden bg-white border border-gray-100  rounded-3xl  shadow ">
+      <Image
+        src={"https:" + coverImage.fields.file.url}
+        alt={coverImage.fields.description}
+        width={396}
+        height={240}
+        className=" object-cover h-[250px]"
+      />
 
+      <div className="p-5">
+        <div className="flex flex-wrap  gap-1 mb-2 items-center">
+          <Link
+            href={{
+              pathname: "/blogs/[category]/[blog_detail]",
+              query: { category: category.fields.slug, blog_detail: slug },
+            }}
+            className="flex  items-center gap-2"
+          >
+            <Image
+              src={"https:" + url}
+              alt={coverImage.fields.description}
+              width={40}
+              height={40}
+              className="rounded-full h-7 w-7 object-cover"
+            />
+            <p className="text-sm text-neutral-700">{author.fields.name}</p>
+          </Link>
+          <span className="text-neutral-500   mx-[6px] font-medium">·</span>
+          <p className="text-sm text-neutral-400">
+            {moment(createdAt).format("MMM DD, YYYY")}
+          </p>
+        </div>
         <Link
           href={{
             pathname: "/blogs/[category]/[blog_detail]",
             query: { category: category.fields.slug, blog_detail: slug },
           }}
-          className="post-card-read-text"
         >
-          Read Article
-          <Arrow height={24} width={24} />
+          <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900  ">
+            {title}
+          </h5>
+
+          <p className="mb-3 font-normal  text-sm text-gray-700 dark:text-gray-400">
+            {excerpt}
+          </p>
         </Link>
       </div>
     </div>
