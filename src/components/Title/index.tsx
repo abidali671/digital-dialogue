07bdbee29cf962db497e351e5678cbefad2dc7d7
abidali671 <abid.saeed.ali92@gmail.com>
2024-09-28 12:08:25 +0500

    update tag component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -9,7 +9,7 @@ const Title = (props: TitleProps) => {
   const { children, as: Tag = "h1" } = props;
   return (
     <span className="flex gap-1 flex-col">
-      <Tag className="font-bold">{children}</Tag>
+      {React.createElement(Tag, { className: "font-bold" }, children)}
       <hr className="w-24 h-[4px] border-1 rounded bg-orange-700" />
     </span>
   );
